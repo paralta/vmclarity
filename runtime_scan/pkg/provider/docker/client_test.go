aafa3ad9535b98cbb1e7adc6e44773012d06fc9a
Catarina Paralta <anacatarinaparalta@gmail.com>
2023-06-26 12:01:44 +0100

    feat(docker): Add basic test for Docker provider client

---
+++ runtime_scan/pkg/provider/docker/client_test.go
@@ -0,0 +1,27 @@
+package docker
+
+import (
+	"context"
+	"github.com/docker/docker/client"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	c := Client{
+		dockerClient: dockerClient,
+		config: &Config{
+			ScannerImage: "ghcr.io/openclarity/vmclarity-cli:latest",
+		},
+	}
+
+	_, err = c.DiscoverAssets(context.Background())
+	if err != nil {
+		panic(err)
+	}
+}
